2b673835705f14e2dda6b461e94b9fa1c0588e2e
vabhi <cvabhi94@gmail.com>
2021-07-08 15:53:45 -0500

    preventDefault

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -6,14 +6,16 @@ class SearchBar extends React.Component {
     // onInputChange (event) {
     //     console.log(event.target.value);
     // }
-
-
+//  prevent default refresh whole browser when submit the form
+    onFormSubmit (event) {
+        event.preventDefault();
+    }
 
 
     render () {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form  onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
                         <input type="text" value= {this.state.term} onChange= {e => this.setState ({term: e.target.value})}  />
