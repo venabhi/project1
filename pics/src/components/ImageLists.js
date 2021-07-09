49fdcb916b8e6d5a6e7666500dab65131ac5b3af
vabhi <cvabhi94@gmail.com>
2021-07-09 13:18:59 -0500

    grid-css

--- pics/src/components/ImageLists.js
+++ pics/src/components/ImageLists.js
@@ -1,11 +1,11 @@
 import React  from 'react';
-
+import './ImageList.css';
 const ImageList = props => {
     const images= props.images.map((image)=> {
         return <img alt={image.description} key={image.id} src= {image.urls.regular} />
     }
     );
-    return <div>{images}</div>
+    return <div className="image-list">{images}</div>
 };
 
 export default ImageList;
