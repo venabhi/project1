7e445ca52cc376b9c4832e92385a9c9157743340
vabhi <cvabhi94@gmail.com>
2021-07-09 10:44:07 -0500

    giving promises

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,15 +3,15 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        axios.get('https://api.unsplash.com/search/photos', {
+    async onSearchSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: {query: term},
             headers: {
                 Authorization: 'Client-ID onpVLSGJfgJ5jirDg6ofRZsB3aRmxLlMq8RVzy8HAc8'
             }
         }
         );
-        // console.log(term);
+        console.log(response.data.results);
     }
     render () {
         return (
